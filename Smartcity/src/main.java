2c5331524e9b75c4e84c44b1f660c2b7a24f8ad5
vishnushedole <84067052+vishnushedole@users.noreply.github.com>
2022-01-03 21:18:08 +0530

    Update main.java

--- Smartcity/src/main.java
+++ Smartcity/src/main.java
@@ -7,51 +7,38 @@ public class main
 {
 	private static JTextField textField;
 	private static JPasswordField passwordField;
-	public static void main(String args[])
+	public main()
 	{
 		JFrame frame = new JFrame("SMART CITY");
-		frame.getContentPane().setBackground(new Color(47, 79, 79));
+		frame.setResizable(false);
+		frame.getContentPane().setBackground(Color.WHITE);
 		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 25));
 		frame.setSize(800,800);
- 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  		
 		ImageIcon image = new ImageIcon("C:\\Users\\vaishnavi\\Downloads/logo.jpg");
 		frame.setIconImage(image.getImage());
-		
-		JLabel title = new JLabel("SMART CITY",JLabel.CENTER);
-		title.setBounds(0, 0, 800, 150);
-		title.setBackground(new Color(50,250,150));
-		title.setText("SMART CITY");
-		title.setFont(new Font("verdana",Font.PLAIN,100));
-		title.setLayout(new BorderLayout());
-		title.setOpaque(true);
-		title.setVerticalAlignment(SwingConstants.CENTER);
 		frame.getContentPane().setLayout(null);
-		
-		
-		frame.getContentPane().add(title);
 		//frame.add(area);
 		frame.getContentPane().setLayout(null);
-		
 		JLabel username = new JLabel("USER NAME");
 		username.setFont(new Font("Verdana", Font.PLAIN, 25));
-		username.setBounds(133, 390, 180, 30);
+		username.setBounds(181, 574, 180, 30);
 		frame.getContentPane().add(username);
 		
 		JLabel lblNewLabel = new JLabel("PASSWORD");
 		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 25));
-		lblNewLabel.setBounds(133, 459, 180, 30);
+		lblNewLabel.setBounds(181, 632, 180, 30);
 		frame.getContentPane().add(lblNewLabel);
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Verdana", Font.PLAIN, 22));
-		textField.setBounds(367, 390, 180, 30);
+		textField.setBounds(487, 575, 180, 30);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		passwordField = new JPasswordField();
 		passwordField.setFont(new Font("Verdana", Font.PLAIN, 22));
-		passwordField.setBounds(368, 459, 180, 30);
+		passwordField.setBounds(487, 633, 180, 30);
 		frame.getContentPane().add(passwordField);
 		
 		JButton btnNewButton = new JButton("LOGIN");
@@ -61,25 +48,23 @@ public class main
 					userframe f = new userframe();
 			}
 		});
-		btnNewButton.setBounds(301, 566, 150, 40);
+		btnNewButton.setBounds(288, 713, 150, 40);
 		frame.getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("LOGIN");
-		btnNewButton_1.setForeground(Color.DARK_GRAY);
-		btnNewButton_1.setBackground(Color.LIGHT_GRAY);
-		btnNewButton_1.setFont(new Font("Verdana", Font.PLAIN, 25));
-		btnNewButton_1.setBounds(217, 160, 133, 48);
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\vaishnavi\\Downloads\\smartC.jpg"));
+		lblNewLabel_1.setBounds(-64, 0, 971, 525);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton_1 = new JButton("USER");
+		btnNewButton_1.setFont(new Font("Arial Black", Font.BOLD, 22));
+		btnNewButton_1.setBounds(265, 516, 121, 30);
 		frame.getContentPane().add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("SIGNIN");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		btnNewButton_2.setBackground(Color.LIGHT_GRAY);
-		btnNewButton_2.setFont(new Font("Verdana", Font.PLAIN, 25));
-		btnNewButton_2.setBounds(398, 160, 133, 48);
+		JButton btnNewButton_2 = new JButton("ADMIN");
+		btnNewButton_2.setFont(new Font("Arial Black", Font.BOLD, 22));
+		btnNewButton_2.setBounds(396, 516, 121, 30);
 		frame.getContentPane().add(btnNewButton_2);
         
 	//	frame.add(content);
